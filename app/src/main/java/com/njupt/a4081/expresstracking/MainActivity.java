6f69629b14ad1268e027e331e1c8684612a4cfe5
Hsu <lem4315063@outlook.com>
2017-10-30 19:46:07 +0800

    Init

---
+++ app/src/main/java/com/njupt/a4081/expresstracking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.njupt.a4081.expresstracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
