9b2229803ac787cc899fe547b667f3ef798b2229
Xty <996335491@qq.com>
2017-11-01 21:17:21 +0800

    make OrderDistinguish version 1.00

--- app/src/main/java/com/njupt/a4081/expresstracking/MainActivity.java
+++ app/src/main/java/com/njupt/a4081/expresstracking/MainActivity.java
@@ -2,6 +2,10 @@ package com.njupt.a4081.expresstracking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SearchView query_searchView = (SearchView)findViewById(R.id.query_searchView);
+        Button search_btn = (Button)findViewById(R.id.search_btn);
+        final TextView result_txtView = (TextView)findViewById(R.id.result_textView);
+
+
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String expNo = query_searchView.getQuery().toString();
+                OrderDistinguish api = new OrderDistinguish();
+                try {
+                    String result = api.getOrderTracesByJson(expNo);
+                    result_txtView.setText(result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
