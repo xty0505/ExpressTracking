f7a3d187347179b313806fde7ecc9191d23b042d
Hsu <lem4315063@outlook.com>
2017-11-05 19:46:57 +0800

    Auto SearchView, some bugs left

--- app/src/main/java/com/njupt/a4081/expresstracking/MainActivity.java
+++ app/src/main/java/com/njupt/a4081/expresstracking/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         NewSearching.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent iNewSearching = new Intent(MainActivity.this, NewSearching.class);
+                Intent iNewSearching = new Intent(MainActivity.this, NewSearchActivity.class);
                 startActivity(iNewSearching);
             }
         });
